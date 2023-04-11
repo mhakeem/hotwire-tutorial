ba01ecede2c1fbab16fa65400ac190cf2ebbe8f3
Mazin Hakeem <mazhak@msn.com>
2023-04-10 21:12:07 -0600

    Add turbo stream and broadcasting mechanism for real time updates

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -11,4 +11,11 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> {
+    broadcast_prepend_to 'quotes',
+    partial: 'quotes/quote',
+    locals: { quote: self },
+    target: 'quotes'
+  }
 end
